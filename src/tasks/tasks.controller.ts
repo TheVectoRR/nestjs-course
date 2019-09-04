f7b42715411e8af1c26f8ab9855ad37b52c2ebc0
Jonathan <jonathanoudshoorn@gmail.com>
2019-09-04 17:40:34 +0200

    connecting to postgres DB

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,6 +3,7 @@ import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDTO } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
@@ -10,8 +11,8 @@ export class TasksController {
     constructor(private tasksService: TasksService) {}
 
     @Get()
-    public getTasks(@Query() filterDto: GetTasksFilterDTO): Task[] {
-        if (Object.keys(filterDto).length){
+    public getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDTO): Task[] {
+        if (Object.keys(filterDto).length) {
             return this.tasksService.getTasksWithFilters(filterDto);
         } else {
             return this.tasksService.getAllTasks();
@@ -37,7 +38,7 @@ export class TasksController {
     @Patch('/:id/status')
     public updateTaskById(
             @Param('id') id: string,
-            @Body('status') status: TaskStatus
+            @Body('status', TaskStatusValidationPipe) status: TaskStatus
         ): Task {
             return this.tasksService.updateTaskById(id, status);
     }
