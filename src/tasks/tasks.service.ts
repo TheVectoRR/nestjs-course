6e15f41b0302248759e4940a42fc69c3250600ac
Jonathan <jonathanoudshoorn@gmail.com>
2019-09-11 18:06:24 +0200

    cleaning up warnings

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,7 +11,7 @@ export class TasksService {
     constructor(
         @InjectRepository(TaskRepository)
         private taskRepository: TaskRepository,
-    ){}
+    ) {}
 
     // public getAllTasks(): Task[] {
     //     return this.tasks;
